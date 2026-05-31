78e5420cd2dcf5d99136db37014157eaf0223c6c
zubairaladzhari <zubairaladzhari@MacBook-Air-zubairaladzhari.local>
2026-05-31 23:06:55 +0700

    Tambah Fitur

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,3 +1,12 @@
 import './style.css'
 
 console.log('Website UMKM aktif')
+
+import './style.css'
+
+const menuButton = document.querySelector('#menuButton')
+const mobileMenu = document.querySelector('#mobileMenu')
+
+menuButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('hidden')
+})
